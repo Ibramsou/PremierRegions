ac01dc0ba57582842fc791d79a1b781c89bd7d47
Ibramsou <kasobanboui@gmail.com>
2024-10-27 17:54:31 +0100

    Initial Commit

---
+++ core/src/main/java/fr/premier/regions/RegionsHandler.java
@@ -0,0 +1,6 @@
+package fr.premier.regions;
+
+import fr.premier.regions.api.PreRegionsAPI;
+
+public class RegionsHandler implements PreRegionsAPI {
+}
---
+++ core/src/main/java/fr/premier/regions/RegionsPlugin.java
@@ -0,0 +1,6 @@
+package fr.premier.regions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RegionsPlugin extends JavaPlugin {
+}
